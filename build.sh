71b10da6224f4c2ba01359be95d6257108ef84c1
Mārtiņš Možeiko <martins.mozeiko@gmail.com>
2024-08-01 08:53:58 -0700

    gcc v14.2.0, gdb v15.1, make v4.4.1

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ MPC_VERSION=1.3.1
 ISL_VERSION=0.26
 EXPAT_VERSION=2.6.2
 BINUTILS_VERSION=2.42
-GCC_VERSION=14.1.0
-MAKE_VERSION=4.2.1
-GDB_VERSION=14.2
+GCC_VERSION=14.2.0
+MAKE_VERSION=4.4.1
+GDB_VERSION=15.1
 
 # set HOST env variable to i686-w64-mingw32 if you want to get 32-bit windows binaries
 HOST=${HOST:-x86_64-w64-mingw32}
@@ -78,7 +78,7 @@ get https://github.com/libexpat/libexpat/releases/download/R_${EXPAT_VERSION//./
 get https://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.xz
 get https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.xz
 get https://ftp.gnu.org/gnu/gdb/gdb-${GDB_VERSION}.tar.xz
-get https://ftp.gnu.org/gnu/make/make-${MAKE_VERSION}.tar.bz2
+get https://ftp.gnu.org/gnu/make/make-${MAKE_VERSION}.tar.gz
 
 curl -Lsf "https://sourceware.org/git/?p=binutils-gdb.git;a=patch;h=45e83f865876e42d22cf4bc242725bb4a25a12e3" | patch -t -N -p1 -d ${SOURCE}/gdb-${GDB_VERSION} || true
 
@@ -264,11 +264,9 @@ find ${FINAL}/lib -name '*.a'   -print0 | xargs -0 -n 8 -P `nproc` ${TARGET}-str
 
 7zr a -mx9 -mqs=on -mmt=on ${OUTPUT}/${NAME}.7z ${FINAL}
 
-if [[ -v GITHUB_WORKFLOW ]]; then
-  echo "::set-output name=GCC_VERSION::${GCC_VERSION}"
-  echo "::set-output name=GDB_VERSION::${GDB_VERSION}"
-  echo "::set-output name=MAKE_VERSION::${MAKE_VERSION}"
-  echo "::set-output name=OUTPUT_BINARY::${NAME}.7z"
-  echo "::set-output name=RELEASE_NAME::gcc-v${GCC_VERSION}"
-  rm -rf "${BUILD}" "${BOOTSTRAP}" "${PREFIX}" "${FINAL}"
+if [[ -v GITHUB_OUTPUT ]]; then
+  echo "GCC_VERSION=${GCC_VERSION}"     >>${GITHUB_OUTPUT}
+  echo "GDB_VERSION=${GDB_VERSION}"     >>${GITHUB_OUTPUT}
+  echo "MAKE_VERSION=${MAKE_VERSION}"   >>${GITHUB_OUTPUT}
+  echo "OUTPUT_BINARY=${NAME}"          >>${GITHUB_OUTPUT}
 fi
